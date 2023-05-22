1ab29b61f64590e214283535da225a8f7ab356ec
aadi753 <aditya.in753@gmail.com>
2023-05-22 13:11:30 +0530

    cmake added and basic file struct made.

---
+++ build/CMakeFiles/Trajectory.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Trajectory.dir/src/cubic.cpp.o"
+  "libTrajectory.pdb"
+  "libTrajectory.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Trajectory.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/trajTest.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/trajTest.dir/bin/cubicTraj.cpp.o"
+  "trajTest"
+  "trajTest.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/trajTest.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
